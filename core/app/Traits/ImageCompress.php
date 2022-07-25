5146a4a1e3d72b1496f31d69577e2295ab722513
M.Ismail Atlasi <miatlasi17@gmail.com>
2022-07-25 07:37:26 -0700

    image fix

--- core/app/Traits/ImageCompress.php
+++ core/app/Traits/ImageCompress.php
@@ -10,35 +10,35 @@ trait ImageCompress {
         $image_full_name =$imagesname.'.'.$ext;
         $upload_path = 'uploads/';
         $image_url= $upload_path.$image_full_name;
-        $thumbnail_path = $upload_path.'thumbnail/'.$image_full_name;
-        $normal_path = $upload_path.'normal/'.$image_full_name;
-        $info = getimagesize($file);
-        if ($info['mime'] == 'image/jpeg')
-          $image = imagecreatefromjpeg($file);
-        elseif ($info['mime'] == 'image/gif')
-          $image = imagecreatefromgif($file);
-        elseif ($info['mime'] == 'image/png')
-          $image = imagecreatefrompng($file);
-        elseif ($info['mime'] == 'image/webp')
-          $image = imagecreatefromwebp($file);
+        $thumbnail_path = $upload_path.'thumbnail/';
+        // $normal_path = $upload_path.'normal/'.$image_full_name;
+        // $info = getimagesize($file);
+        // if ($info['mime'] == 'image/jpeg')
+        //   $image = imagecreatefromjpeg($file);
+        // elseif ($info['mime'] == 'image/gif')
+        //   $image = imagecreatefromgif($file);
+        // elseif ($info['mime'] == 'image/png')
+        //   $image = imagecreatefrompng($file);
+        // elseif ($info['mime'] == 'image/webp')
+        //   $image = imagecreatefromwebp($file);
 
 
-        if($info[0] > 1000 && $info[0] < 2000){
-            imagewebp($image, $thumbnail_path, 40);
-            imagewebp($image, $normal_path, 60);
-        }
-        else if($info[0] >= 2000 && $info[0] < 3000){
-            imagewebp($image, $thumbnail_path, 30);
-            imagewebp($image, $normal_path, 50);
-        }
-        else if($info[0] >= 3000 ){
-            imagewebp($image, $thumbnail_path, 20);
-            imagewebp($image, $normal_path, 40);
-        }else{
-            imagewebp($image, $thumbnail_path, 80);
-            imagewebp($image, $normal_path, 90);
-        }
-        $file->move($upload_path,$image_full_name);
+        // if($info[0] > 1000 && $info[0] < 2000){
+        //     imagewebp($image, $thumbnail_path, 40);
+        //     imagewebp($image, $normal_path, 60);
+        // }
+        // else if($info[0] >= 2000 && $info[0] < 3000){
+        //     imagewebp($image, $thumbnail_path, 30);
+        //     imagewebp($image, $normal_path, 50);
+        // }
+        // else if($info[0] >= 3000 ){
+        //     imagewebp($image, $thumbnail_path, 20);
+        //     imagewebp($image, $normal_path, 40);
+        // }else{
+        //     imagewebp($image, $thumbnail_path, 80);
+        //     imagewebp($image, $normal_path, 90);
+        // }
+        $file->move($thumbnail_path,$image_full_name);
         return $image_full_name;
   }
 
